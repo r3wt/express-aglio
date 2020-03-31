7146f42953ff36fef7c34706250e81a0bf1bdf74
Garrett Morris <gmorris_89@outlook.com>
2020-03-31 18:02:49 -0500

    refactoring

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const _ = require('lodash');
-const await = require('async-await-callables'); // see https://www.npmjs.com/package/async-await-callables for api
 const aglio = require('aglio');
 const watch = require('node-watch');
 const path = require('path');
@@ -22,14 +20,88 @@ const _options = {
 };
 
 
-module.exports = function( app, options ) {
+function validateOptions(options){
+	
+	var sourceStat = fs.statSync(options.source);
+	
+	if(!sourceStat.isDirectory() && !sourceStat.isFile()){
+		throw new Error('express-aglio: options.source is not a file or directory. check path and try again.');
+	}
+	
+	var outputStat = fs.statSync(options.output);
+	
+	if(!outputStat.isDirectory() && !outputStat.isFile()){
+		throw new Error('express-aglio: options.output is not a file or directory. check path and try again.');
+	}
+	
+	['watch','debug','expose'].forEach(function(prop){
+		if(typeof options[prop] !== 'boolean'){
+			throw new Error( 'express-aglio: options.%s must be a boolean value (true/false).'.replace('%s',prop) );
+		}
+	});
+	
+	if(typeof options.log !== 'function'){
+		throw new Error('express-aglio: options.log must be a function. to disable built in logger use options.debug = false.');
+	}
+	
+};
+
+
+function generateFileList(options,callback){
+	
+	var sourceIsDir = fs.lstatSync(options.source).isDirectory();
+	var outputIsDir = fs.lstatSync(options.output).isDirectory();
+    
+    options.serveDir = options.output;
+	if(!outputIsDir){
+		options.serveDir = options.serveDir.split('/');
+		options.serveDir.pop();
+		options.serveDir = options.serveDir.join('/') +'/';
+	}
+	
+	if(sourceIsDir && outputIsDir){
+		
+		fs.readdir(options.source,function(err,files){
+			if(err)
+				options.log('express-aglio: error reading source directory.');
+			else
+				var fileList = [];
+				files.forEach(function(file){
+					if(file.slice((file.lastIndexOf(".") - 1 >>> 0) + 2) == 'apib'){
+						fileList.push({
+							source: file,
+							output: file.replace('.apib','.html')
+						});
+					}
+				});
+				callback(fileList);
+		});
+		
+	}
+	else if(!sourceIsDir && !outputIsDir){
+		
+		var fileList = [{
+			source: options.source,
+			output: options.output,
+		}];
+		
+		callback(fileList);
+		
+	}else{
+		throw new Error('express-aglio: output and source must both be either directories or single files.');
+	}
+};
+
+
+
+module.exports = function( app, __options ) {
 	
 	//first step: parse and validate options.
-	options = _.extend({},_options,options);	
+	const options = {..._options,...__options};	
 	
-	require('./lib/validateOptions')(options);
+	validateOptions(options);
     
-    var outputIsDir = fs.lstatSync(options.output).isDirectory();
+    const outputIsDir = fs.lstatSync(options.output).isDirectory();
     options.serveDir = options.output;
     
     if(!outputIsDir){
@@ -40,49 +112,37 @@ module.exports = function( app, options ) {
     
     //mount doc route immediately
     if(options.expose){
-        var express = require('express');
-        options.log(options);
+        const express = require('express');
+        options.log('express-aglio: started with options:',options);
         app.use(options.uri,express.static(options.serveDir));
     }
 	
 	
 	//second step: generate the file list
-	require('./lib/generateFileList')(options,function(fileList){
+	generateFileList(options,fileList=>{
+
+        const buildFn = () => {
+            return Promise.all(fileList.map(fStruct=>{
+			
+                return new Promise((resolve,reject)=>{
+                    aglio.renderFile( fStruct.source, fStruct.output,options.aglioOptions,function (err, warnings) {
+                        if(err){
+                            options.log('express-aglio: error building doc from source file `%s`'+"\r\n",fStruct.source);
+                            options.log('express-aglio: aglio error details '+"\r\n");
+                            options.log(err);
+                            reject(err);
+                        }else{
+                            resolve();
+                        }
+                    });
+                });
+
+
+            }));
+        }
 		
 		//third step: generate the build step
-		var build = [];
-		fileList.forEach(function(fStruct){
-			build.push(function(next){
-					aglio.renderFile( fStruct.source, fStruct.output,options.aglioOptions,function (err, warnings) {
-					if(err){
-						options.log('express-aglio: error building doc from source file `%s`'+"\r\n",fStruct.source);
-						options.log('express-aglio: aglio error details '+"\r\n");
-						options.log(err);
-						next(err);
-					}else{
-						next();
-					}
-				});	
-			});
-		});
-		
-		//fourth step: run first build
-		var buildFn = function(next){ 
-		
-			await(build,function(errors,results){
-				
-				if(errors){
-					options.log('express-aglio: error building docs; some docs did not compile successfully.');
-				}else{
-					options.log('express-aglio: docs built successfully');
-				}
-				if(typeof next == 'function') next(); //closes #1
-			});
-			
-		};
-		
-		//fifth step: make first build
-		await([buildFn],function(){
+		buildFn().then(()=>{
             options.log('express-aglio: built docs(startup)');
 			//sixth and final step: setup watch and routes
 			if(options.watch){
@@ -90,8 +150,7 @@ module.exports = function( app, options ) {
 				watch(path.dirname(options.source),buildFn);
 				options.log('express-aglio: watching filesystem for changes');
 			}
-			
-		});
+        });
 		
 	});
 	
